46660174f92d2e1b997a3f4dfb6232775963781e
VasishthG <76578694+vasishthg@users.noreply.github.com>
2023-08-30 12:11:38 +0530

    e

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
@@ -23,10 +27,195 @@ func main() {
 	}
 	defer db.Close()
 	router.GET("/", func(c *gin.Context) {
+		session := sessions.Default(c)
+		fmt.Println(session.Get("loggedin"))
+		if session.Get("loggedin") != true {
+			c.Redirect(http.StatusTemporaryRedirect, "/login")
+			return
+		}
+		// agent := struct {
+		// 	ID       int    `json:"id"`
+		// 	Name     string `json:"name"`
+		// 	Alias    string `json:"alias"`
+		// 	password string `json:"password"`
+		// 	pfp      string `json:"pfp"`
+		// 	files    string `json:"files"`
+		// 	missions string `json:"missions"`
+		// 	code     string `json:"code"`
+		// 	level    int    `json:"level"`
+		// }
 		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
+	router.GET("/chat", func(c *gin.Context) {
+		session := sessions.Default(c)
+		fmt.Println(session.Get("loggedin"))
+		if session.Get("loggedin") != true {
+			c.Redirect(http.StatusTemporaryRedirect, "/login")
+			return
+		}
+		c.HTML(http.StatusOK, "chat.html", gin.H{})
+	})
+	router.GET("/map", func(c *gin.Context) {
+		session := sessions.Default(c)
+		fmt.Println(session.Get("loggedin"))
+		if session.Get("loggedin") != true {
+			c.Redirect(http.StatusTemporaryRedirect, "/login")
+			return
+		}
+		c.HTML(http.StatusOK, "map.html", gin.H{})
+	})
+
 	router.GET("/login", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "login.html", gin.H{})
+		session := sessions.Default(c)
+		fmt.Println(session.Get("loggedin"))
+		// login := struct {
+		// 	Username string `json:"username"`
+		// 	Password string `json:"password"`
+		// 	Code     string `json:"code"`
+		// }{}
+
+		if session.Get("loggedin") != true {
+			c.HTML(http.StatusOK, "login.html", gin.H{})
+		} else {
+			c.Redirect(http.StatusTemporaryRedirect, "/")
+
+			return
+		}
+	})
+	router.GET("/resources", func(c *gin.Context) {
+		session := sessions.Default(c)
+		fmt.Println(session.Get("loggedin"))
+		if session.Get("loggedin") != true {
+			c.Redirect(http.StatusTemporaryRedirect, "/login")
+			return
+		}
+		c.HTML(http.StatusOK, "resources.html", gin.H{})
+	})
+	router.GET("/request", func(c *gin.Context) {
+		session := sessions.Default(c)
+		fmt.Println(session.Get("loggedin"))
+		if session.Get("loggedin") != true {
+			c.Redirect(http.StatusTemporaryRedirect, "/login")
+			return
+		}
+		c.HTML(http.StatusOK, "request.html", gin.H{})
+	})
+
+	router.POST("/login/verify", func(c *gin.Context) {
+		session := sessions.Default(c)
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+
+		type agent struct {
+			ID       int    `json:"id"`
+			Name     string `json:"name"`
+			Alias    string `json:"alias"`
+			Password string `json:"password"`
+			Pfp      string `json:"pfp"`
+			Files    []byte `json:"files"`
+			Missions []byte `json:"missions"`
+			Code     string `json:"code"`
+			Level    int    `json:"level"`
+		}
+
+		Agent := agent{}
+
+		code := session.Get("code")
+
+		err := db.QueryRow("SELECT id, name, alias, password, pfp, files, missions, code, level FROM agent WHERE code = ?", code).Scan(
+			&Agent.ID, &Agent.Name, &Agent.Alias, &Agent.Password, &Agent.Pfp, &Agent.Files, &Agent.Missions, &Agent.Code, &Agent.Level)
+		if err != nil {
+			fmt.Println(err)
+		}
+		AgentStr := strconv.Itoa(Agent.ID)
+		if username != AgentStr {
+			c.JSON(http.StatusConflict, gin.H{"error": "Agent cipher different from ID"})
+		} else {
+			err := db.QueryRow("SELECT id, name, alias, password, pfp, files, missions, code, level FROM agent WHERE id = ?", username).Scan(
+				&Agent.ID, &Agent.Name, &Agent.Alias, &Agent.Password, &Agent.Pfp, &Agent.Files, &Agent.Missions, &Agent.Code, &Agent.Level)
+			if err != nil {
+				fmt.Println(err)
+			}
+			if Agent.Password == password {
+				session.Set("loggedin", true)
+				session.Set("name", Agent.Name)
+				session.Set("alias", Agent.Alias)
+				session.Set("pfp", Agent.Pfp)
+				session.Set("files", Agent.Files)
+				session.Set("missions", Agent.Missions)
+				session.Set("code", Agent.Code)
+				session.Set("level", Agent.Level)
+				session.Save()
+				c.Redirect(http.StatusTemporaryRedirect, "/")
+			} else {
+				c.JSON(http.StatusExpectationFailed, gin.H{"error": "Invalid password"})
+			}
+		}
+	})
+	router.POST("/upload", func(c *gin.Context) {
+		file, _ := c.FormFile("file")
+		if err != nil {
+			fmt.Println(err)
+		}
+		log.Println(file.Filename)
+		session := sessions.Default(c)
+		c.SaveUploadedFile(file, "/static/uploads/files/"+file.Filename)
+		files = []
+		_, err := db.Exec("UPDATE agent SET files = ? WHERE id = ?", file.Filename, session.Get("ID"))
+		if err != nil {
+			fmt.Println(err)
+		}
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+	})
+
+	router.POST("/check/cipher", func(c *gin.Context) {
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "No file uploaded"})
+			return
+		}
+		if !strings.HasSuffix(file.Filename, ".txt") {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Only .txt files are allowed"})
+			return
+		}
+		uploadedFile, err := file.Open()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error opening file"})
+			return
+		}
+		defer uploadedFile.Close()
+		content, err := ioutil.ReadAll(uploadedFile)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error reading file content"})
+			return
+		}
+		processedContent := string(content)
+		type agent struct {
+			ID       int    `json:"id"`
+			Name     string `json:"name"`
+			Alias    string `json:"alias"`
+			Password string `json:"password"`
+			Pfp      string `json:"pfp"`
+			Files    string `json:"files"`
+			Missions string `json:"missions"`
+			Code     string `json:"code"`
+			Level    int    `json:"level"`
+		}
+		Agent := agent{}
+		err = db.QueryRow("SELECT id, name, alias, password, pfp, files, missions, code, level FROM agent WHERE code = ?", processedContent).Scan(&Agent.ID, &Agent.Name, &Agent.Alias, &Agent.Password, &Agent.Pfp, &Agent.Files, &Agent.Missions, &Agent.Code, &Agent.Level)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if Agent.Code == processedContent {
+			session := sessions.Default(c)
+			session.Set("ID", Agent.ID)
+			session.Set("code", processedContent)
+			session.Save()
+		} else {
+			c.JSON(http.StatusConflict, gin.H{"error": "Invalid code"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"code": processedContent})
 	})
 
 	router.Run(":5500")
--- static/main.js
+++ static/main.js
@@ -1,7 +1,179 @@
 const myFunction = () => {
-    var time = new Date().getHours() + ":" + new Date().getMinutes();
-    console.log(time);
+    mins = new Date().getMinutes();
+    if (mins < 10) {
+        mins = "0" + mins;
+    }
+    var time = new Date().getHours() + ":" + mins;
     $("#dash-time-txt").text(time);
+
 };
 
+$(".goto-chat").click(function(){
+        $(".loading").fadeIn();
+    setTimeout(function(){
+        history.pushState({}, null, "/chat")
+        document.title = "Probe / Chat";
+
+        document.querySelector("body").innerHTML='<object class="embed" type="text/html" data="/chat" ></object>';
+        setTimeout(function(){
+                $(".loading").fadeOut();
+        }, 3000)
+    }, 2000)
+})
+$(".goto-map").click(function(){
+    $(".loading").fadeIn();
+setTimeout(function(){
+    history.pushState({}, null, "/map")
+    document.title = "Probe / Map";
+
+    document.querySelector("body").innerHTML='<object class="embed" type="text/html" data="/map" ></object>';
+    setTimeout(function(){
+            $(".loading").fadeOut();
+    }, 3000)
+}, 2000)
+})
+$(".goto-home").click(function(){
+    $(".loading").fadeIn();
+setTimeout(function(){
+    history.pushState({}, null, "/")
+    document.title = "Probe / Home";
+    document.querySelector("body").innerHTML='<object class="embed" type="text/html" data="/" ></object>';
+    setTimeout(function(){
+            $(".loading").fadeOut();
+    }, 3000)
+}, 2000)
+})
+
+$(".goto-resources").click(function(){
+    $(".loading").fadeIn();
+setTimeout(function(){
+    history.pushState({}, null, "/resources")
+    document.title = "Probe / Resources";
+    document.querySelector("body").innerHTML='<object class="embed" type="text/html" data="/resources" ></object>';
+    setTimeout(function(){
+            $(".loading").fadeOut();
+    }, 3000)
+}, 2000)
+})
+
+
+$(".goto-request").click(function(){
+    $(".loading").fadeIn();
+setTimeout(function(){
+    history.pushState({}, null, "/request")
+    document.title = "Probe / Request";
+    document.querySelector("body").innerHTML='<object class="embed" type="text/html" data="/request" ></object>';
+    setTimeout(function(){
+            $(".loading").fadeOut();
+    }, 3000)
+}, 2000)
+})
+function ijwei(){
+    $(".login-arow").animate({
+        width: "100%",
+        backgroundColor: "#c4c4c4",
+        left: "0",
+        margin: "0 0 0 0",
+        height: "4em"
+    }, 1000)
+}
+$(".login-submit").click(function(){
+    event.preventDefault()
+    
+    $.ajax({
+        url: "/login/verify",
+        type: "POST",
+        data: {
+            username: parseInt($("#username").val()),
+            password: $("#password").val()
+        },
+        success: function(response) {
+            ijwei()
+            setTimeout(function(){
+                window.location.href = "/"
+            }, 1500)
+            console.log(response)
+        },
+        error: function(error) {
+            if (error.status === 409){
+                $(".success-cipher").fadeIn(500);
+                $("#sc-title").text("Agent cipher different from ID")
+                setTimeout(function(){
+                    $(".success-cipher").fadeOut();
+                }, 4000)
+            }
+            if (error.status === 417){
+                $(".success-cipher").fadeIn(500);
+                $("#sc-title").text("Incorrect password")
+                setTimeout(function(){
+                    $(".success-cipher").fadeOut();
+                }, 4000)
+            }else{
+                console.log(error)
+                
+            }
+            console.log(error)
+                // $(".success-cipher").fadeIn(500);
+                // $("#sc-title").text("Incorrect information")
+                // setTimeout(function(){
+                //     $(".success-cipher").fadeOut();
+                // }, 4000)
+        }
+})
+})
+
+$(".login-form").submit(function(){
+    event.preventDefault()
+    e.preventDefault()
+})
+
+
+
+$(".cipher-submit").click(function(){
+    event.preventDefault()
+    var fileInput = $("#upload")[0];
+    if (fileInput.files.length > 0) {
+        var file = fileInput.files[0];
+        if (file.name.endsWith(".txt")) {
+            var formData = new FormData();
+            formData.append("file", file);
+
+            $.ajax({
+                url: "/check/cipher",
+                type: "POST",
+                data: formData,
+                processData: false,
+                cache: false,
+                contentType: false,
+                success: function(response) {
+                    $(".cipher-upload").fadeOut();
+                    console.log(response)
+                    setTimeout(function(){
+                        $(".success-cipher").fadeIn(500);
+                        setTimeout(function(){
+                            $(".success-cipher").fadeOut();
+                        }, 4000)
+                    })
+                },
+                error: function(error) {
+                    if (error.status === 409){
+                        console.log(error)
+                        setTimeout(function(){
+                            $(".success-cipher").fadeIn(500);
+                            $("#sc-title").text("Incorrect cipher")
+                            setTimeout(function(){
+                                $(".success-cipher").fadeOut();
+                            }, 4000)
+                        })
+                    } 
+                    else{
+                        console.log(error)
+                    }
+                }
+            });
+        } else {
+            console.log("Please select a .txt file");
+        }
+    }
+})
 setInterval(myFunction, 100); 
